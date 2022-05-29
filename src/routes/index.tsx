29f2b8d21b020f95bd92e26fcb359ab41636e14d
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2022-05-29 10:50:16 -0300

    tema claro adicionado e o provider em volta do app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
     return(
         <Routes>
             <Route path="/pagina-inicial" element={<p>teste de rota pagina inicial</p>}/>
+            <Route path="/pagina-login" element={<Button variant="contained" color="primary">Login</Button>}/>
             
             {/* redireciona para a página inicial caso não atenda nenhuma das rotas a cima */}
             <Route path="*" element={<Navigate to="/pagina-inicial"/>} />
